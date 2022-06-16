de4fb574da8c6650e6898960bc7adac6ddc79219
Mostafa Negim <mostafa.negim@gmail.com>
2022-06-16 20:33:12 +0000

    17->18

--- steps.sh
+++ steps.sh
@@ -186,3 +186,16 @@ openssl s_client -connect  localhost:30001
 nmap -sV -p 31000-32000 localhost
 echo "cluFn7wTiGryunymYOu4RcffSxQluehd" | openssl s_client -quiet -connect localhost:31790
 # cluFn7wTiGryunymYOu4RcffSxQluehd
+
+# 17 -> 18
+# bandit17
+
+# There are 2 files in the homedirectory: passwords.old and passwords.new. 
+# The password for the next level is in passwords.new and is the only line 
+# that has been changed between passwords.old and passwords.new
+
+# NOTE: if you have solved this level and see ‘Byebye!’ when trying to log into bandit18, 
+# this is related to the next level, bandit19
+
+diff passwords.new passwords.old
+# kfBf3eYk5BPBRzwjqutbbfE887SVc5Yd
