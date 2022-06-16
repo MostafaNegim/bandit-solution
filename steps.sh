e7c93aea99495ac9247c6913e71d34153ad9ef43
Mostafa Negim <mostafa.negim@gmail.com>
2022-06-16 18:43:05 +0000

    15->16

--- steps.sh
+++ steps.sh
@@ -161,3 +161,14 @@ cat /etc/bandit_pass/bandit14
 nc localhost 30000
 4wcYUJFw0k0XLShlDzztnTBHiqxU3b3e
 # BfMYroe26WYalil77FoDi9qh59eK5xNr
+
+# 15 -> 16
+# bandit15
+# The password for the next level can be retrieved by submitting the password of the current 
+# level to port 30001 on localhost using SSL encryption.
+
+# Helpful note: Getting “HEARTBEATING” and “Read R BLOCK”? 
+# Use -ign_eof and read the “CONNECTED COMMANDS” section in the manpage.
+#  Next to ‘R’ and ‘Q’, the ‘B’ command also works in this version of that command…
+openssl s_client -connect  localhost:30001
+# cluFn7wTiGryunymYOu4RcffSxQluehd
